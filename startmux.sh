f4c5d8a6f6e607fad157333d844d1bc8c5e8687d
Afiq <afiqquraisyzulkarnain@gmail.com>
2023-05-04 09:12:42 +0800

    feat: add page 2

--- startmux.sh
+++ startmux.sh
@@ -12,6 +12,17 @@ colorbg() {
     printf "\x1b[48;5;$1m"
 }
 
+# preset colors
+BLACK="$(colorfg 0)" ; LBLACK="$(colorfg 8)"
+RED="$(colorfg 1)" ; LRED="$(colorfg 9)"
+GREEN="$(colorfg 2)" ; LGREEN="$(colorfg 10)"
+YELLOW="$(colorfg 3)" ; LYELLOW="$(colorfg 11)"
+BLUE="$(colorfg 4)" ; LBLUE="$(colorfg 12)"
+MAGENTA="$(colorfg 5)" ; LMAGENTA="$(colorfg 13)"
+CYAN="$(colorfg 6)" ; LCYAN="$(colorfg 14)"
+GREY="$(colorfg 7)" ; LGREY="$(colorfg 15)"
+RESET="$(printf "\x1b[0m")"
+
 hidecur() {
     printf "\x1b[?25l"
 }
@@ -28,32 +39,13 @@ disable_altbuff() {
     printf "\x1b[?1049l"
 }
 
-# preset colors
-BLACK="$(colorfg 0)"
-LBLACK="$(colorfg 8)"
-RED="$(colorfg 1)"
-LRED="$(colorfg 9)"
-GREEN="$(colorfg 2)"
-LGREEN="$(colorfg 10)"
-YELLOW="$(colorfg 3)"
-LYELLOW="$(colorfg 11)"
-BLUE="$(colorfg 4)"
-LBLUE="$(colorfg 12)"
-MAGENTA="$(colorfg 5)"
-LMAGENTA="$(colorfg 13)"
-CYAN="$(colorfg 6)"
-LCYAN="$(colorfg 14)"
-GREY="$(colorfg 7)"
-LGREY="$(colorfg 15)"
-RESET="$(printf "\x1b[0m")"
-
-# tell <msg...>
-tell() {
-    IFS=' ' read -ra words <<< "$1"
+# raw_tell <msg...>
+raw_tell() {
+    IFS=' ' read -ra words <<< "$@"
 
     local n=1
     for w in "${words[@]}"; do
-        printf " ${LYELLOW}$w%s$RESET" ""
+        printf " $COLOR$w%s" "$RESET"
         if (( n >= 8 && ${#w} > 3 )); then
             n=1
             echo
@@ -62,20 +54,42 @@ tell() {
     echo
 }
 
+# tell <msg...>
+tell() { COLOR="$LYELLOW" raw_tell "$@"; }
+
+# run_cmd <cmd> [args...]
+run_cmd() {
+    local cmd="$1"; shift
+
+    COLOR="$LBLUE" raw_tell "\$ $cmd $*\n"
+    "$cmd" "$@" || {
+        COLOR="$RED" raw_tell "Command failed miserably, exit code $?."
+        end_p
+        on_exit 1
+    }
+}
+
 end_p() {
     echo
-    read -rs -p " Press ${LRED}any key$RESET to continue" -N1
+    read -rsp " Press ${LRED}any key$RESET to continue" -N1
     clear
 }
 
 # exit handler
 on_exit() {
     showcur
-    "$EXIT" || clear
     disable_altbuff
+    [[ -n "$1" ]] && exit "$1"
 }
 
-trap on_exit SIGINT
+# SIGINT handler
+sigint() {
+    clear
+    on_exit
+    exit 130
+}
+
+trap sigint SIGINT
 
 #==================================================================#
 
@@ -87,19 +101,32 @@ p1() {
 
     for c in {9..15}; do
         # shellcheck disable=SC2059 #: no, i want ansi codes to work
-        printf -- "  -[ $(colorfg "$c")S T A R T M U X $RESET]-\r"
-        sleep .1
+        printf -- "  <[ $(colorfg "$c")S T A R T M U X$RESET ]>\r"
+        sleep .07
         # shellcheck disable=SC2059 #: same as above
-        printf -- "  -[ $(colorfg "$(( c - 7 ))")S T A R T M U X $RESET]-\r"
-        sleep .1
+        printf -- "  <[ $(colorfg "$(( c - 7 ))")S T A R T M U X$RESET ]>\r"
+        sleep .07
     done; echo $'\n'
 
-    tell "Welcome to ${BLUE}Startmux$RESET$LYELLOW, you're currently running an interactive setup on getting started with ${LBLACK}Termux$RESET!"
+    tell "Welcome to ${BLUE}Startmux$LYELLOW, you're currently running an interactive setup that helps you getting started with ${LBLACK}Termux$RESET!"
 }
+
+p2() {
+    echo
+
+    tell "Let's switch our mirror to something more fast."
+
+    for sec in {3..1..-1}; do
+        printf " ${LYELLOW}Running termux-change-repo in $BLUE%d$LYELLOW...$RESET\r" "$sec"
+        sleep 1
+    done; echo
+
+    run_cmd termux-change-repo
+}
+    
 
 # main section
 p1; end_p
+p2; end_p
 
-
-sleep .5
-EXIT=true on_exit
+on_exit
